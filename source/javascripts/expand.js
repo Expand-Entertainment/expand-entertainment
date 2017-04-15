eb1800cc4103412e53bd6421f236cdc2128d8ead
doodybrains <doodybrainz@gmail.com>
2017-04-15 13:00:12 -0400

    Add js libs, add animation, begin responsive

--- source/javascripts/expand.js
+++ source/javascripts/expand.js
@@ -5,6 +5,14 @@ var Expand = {
     bind: function() {
       Expand.resizeHandler();
 
+      $("#homepage").one('mousemove', function(e) {
+        Expand.expandLinks(e);
+        $(".talent").animate({left: '0', top: '0'}, 400);
+        $(".about").animate({right: '0', top: '0'}, 400);
+        $(".creative").animate({left: '0', bottom: '0'}, 400);
+        $(".contact").animate({right: '0', bottom: '0'}, 400);
+      });
+
       $('#toggle').click(function() {
         Expand.toggleTalent();
       })
@@ -17,5 +25,10 @@ var Expand = {
     toggleTalent: function() {
       console.log('toggle');
       $("div.talent-container").toggleClass("grid");
+    },
+
+    expandLinks: function(e) {
+      e.stopPropagation();
+      $('#home-container').addClass('expanded');
     }
 };
